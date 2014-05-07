90c180b76d89f4af8b37511a689822cb9ef44d4b
Gautam Kotian <gautam.kotian@gmail.com>
2014-05-07 22:43:37 +0200

    Call work specific stuff
    The files containing this "stuff" are deliberately not checked in.

--- home/gautam/g_startup.sh
+++ home/gautam/g_startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# Change resolution according to the monitors at work
-xrandr --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate left --output DP-1 --mode 2560x1440 --pos 0x488 --rotate normal
-
-# Ask for my passphrase at every login
-ssh-add ~/.ssh/id_rsa_sociomantic
+# Sociomantic specific startup.
+FILE=/home/gautam/gautam_linux/g_startup_sociomantic
+if [ -f $FILE ]; then
+    $FILE
+fi
 
