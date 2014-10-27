322aec41fb2c9c781c15140a0e8ffcf90fcdbde0
Gautam Kotian <gautam.kotian@gmail.com>
2014-10-27 10:50:19 +0100

    startup: Open TODO file if it exists

--- home/gautam/g_startup.sh
+++ home/gautam/g_startup.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 GL_DIR=/home/gautam/gautam_linux
+TODO_FILE=/home/gautam/tmp/TODO.txt
 
 # Sociomantic specific startup.
 FILE=$GL_DIR/g_startup_sociomantic
 if [ -f $FILE ]; then
     $FILE
+fi
+
+# Open the todo file, if it exists.
+if [ -f $TODO_FILE ]; then
+    gedit $TODO_FILE
 fi
 
