85614b01681fb33de1b14eefaaf44bc96f839bca
Gautam Kotian <gautam.kotian@gmail.com>
2015-06-17 09:55:30 +0200

    Make startup operations non-blocking

--- scripts/g_startup.sh
+++ scripts/g_startup.sh
@@ -7,11 +7,11 @@ TODO_FILE=/home/gautam/tmp/TODO.txt
 # Sociomantic specific startup.
 FILE=$GL_DIR/g_startup_sociomantic
 if [ -f $FILE ]; then
-    $FILE
+    $FILE &
 fi
 
 # Open the todo file, if it exists.
 if [ -f $TODO_FILE ]; then
-    gedit $TODO_FILE
+    gedit $TODO_FILE &
 fi
 
