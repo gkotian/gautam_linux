100d601fdd7781390735680350ac3d40af02ff9d
Gautam Kotian <gautam.kotian@gmail.com>
2015-10-22 10:55:10 +0200

    Add functions to convert from/to unix time

--- zsh/customizations/lib_patches.zsh
+++ zsh/customizations/lib_patches.zsh
@@ -7,3 +7,19 @@ alias -g ..........='../../../../../../../../..'
 # Alias to go to the root directory of the current git repository
 alias cdr='cd $(git rev-parse --show-toplevel || echo ".")'
 
+function tounixtime () {
+    if [ $# -eq 0 ]; then
+        date -u +%s
+    else
+        date -u -d"$@" +%s
+    fi
+}
+
+function fromunixtime () {
+    if [ $# -eq 0 ]; then
+        date -u
+    else
+        date -u -d@$@
+    fi
+}
+
