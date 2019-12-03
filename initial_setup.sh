988141735dadfd2d1b3ebcd8a3ccad23ec8ab33d
Gautam Kotian <gkotian@users.noreply.github.com>
2019-12-03 12:31:38 +0100

    Change SSH key algorithm from RSA to ED25519
    These are supposed to result in more secure but shorter keys.

--- initial_setup.sh
+++ initial_setup.sh
@@ -145,7 +145,7 @@ chsh -s /bin/zsh ${THE_USER}
 echo "Done!"
 
 echo -n "Setting up ssh to access GitHub... "
-sudo -u ${THE_USER} ssh-keygen -t rsa -b 4096 -f /home/$THE_USER/.ssh/id_rsa_play
+sudo -u ${THE_USER} ssh-keygen -t ed25519 -f /home/$THE_USER/.ssh/id_ed25519_play
 
 echo "Now log in to GitHub -> Settings -> SSH and GPG keys -> New SSH key"
 echo "In 'Title' enter some text to identify this computer"
