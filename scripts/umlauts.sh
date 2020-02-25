b993d9899292acacb4d000c44ac5d5c90fe48f4d
Gautam Kotian <gkotian@users.noreply.github.com>
2020-02-25 21:41:57 +0100

    Add the uppercase umlauts

--- scripts/umlauts.sh
+++ scripts/umlauts.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "ä    ö    ü    ß    €    ₹"
+    echo "ä    ö    ü    Ä    Ö    Ü    ß    €    ₹"
 else
     case $1 in
         a) CHAR="ä" ;;
         o) CHAR="ö" ;;
         u) CHAR="ü" ;;
+        A) CHAR="Ä" ;;
+        O) CHAR="Ö" ;;
+        U) CHAR="Ü" ;;
         s) CHAR="ß" ;;
         e) CHAR="€" ;;
         r) CHAR="₹" ;;
-        *) echo "unrecognized option '$1'... [ä, ö, ü, ß, €, ₹]"
+        *) echo "unrecognized option '$1'... [ä, ö, ü, Ä, Ö, Ü, ß, €, ₹]"
            exit 1 ;;
     esac
 
