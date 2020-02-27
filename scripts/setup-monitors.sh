01b72cef2ec977c1820b71856689fee85c06e90f
Gautam Kotian <gkotian@users.noreply.github.com>
2020-02-27 09:53:06 +0100

    Add some log messages

--- scripts/setup-monitors.sh
+++ scripts/setup-monitors.sh
@@ -5,11 +5,20 @@
 # following content:
 # KERNEL=="card0", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/path/to/.Xauthority", RUN+="/path/to/setup-monitors.sh"
 
+exec 1>>/var/tmp/setup-monitors.log 2>&1
+
+echo "[`date`] setup-monitors.sh triggered"
+
 if xrandr | grep "HDMI-1 disconnected"; then
+    # Disconnecting triggers a message automatically, so we don't need an
+    # explicit one here.
+
     xrandr --output eDP-1  --mode 1920x1080 --pos 0x379  --rotate normal --primary \
            --output HDMI-1 --off \
            --output DP-1   --off \
            --output HDMI-2 --off
+
+    echo "Moving all workspaces to the laptop screen"
 
     i3-msg --quiet '[workspace=1] move workspace to output eDP-1'
     i3-msg --quiet '[workspace=2] move workspace to output eDP-1'
@@ -22,10 +31,14 @@ if xrandr | grep "HDMI-1 disconnected"; then
     i3-msg --quiet '[workspace=9] move workspace to output eDP-1'
     i3-msg --quiet '[workspace=10] move workspace to output eDP-1'
 else
+    echo "HDMI-1 connected"
+
     xrandr --output eDP-1  --mode 1920x1080 --pos 0x379  --rotate normal --primary \
            --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal \
            --output DP-1   --off \
            --output HDMI-2 --off
+
+    echo "Moving selected workspaces to the monitor"
 
     i3-msg --quiet '[workspace=1] move workspace to output HDMI-1'
     i3-msg --quiet '[workspace=2] move workspace to output HDMI-1'
@@ -33,3 +46,6 @@ else
     i3-msg --quiet '[workspace=7] move workspace to output HDMI-1'
     i3-msg --quiet '[workspace=8] move workspace to output HDMI-1'
 fi
+
+echo "done"
+echo "-------------------------------------------------------------------------"
