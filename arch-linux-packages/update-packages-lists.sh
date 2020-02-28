d057cad8d02da7a23f530e9a500507f759aa59ba
Gautam Kotian <gkotian@users.noreply.github.com>
2020-02-28 10:49:36 +0100

    Auto commit changes to package lists

--- arch-linux-packages/update-packages-lists.sh
+++ arch-linux-packages/update-packages-lists.sh
@@ -7,10 +7,12 @@ exec 1>>/var/tmp/update-packages-lists.log 2>&1
 
 echo "[`date`]"
 
-DIRNAME=$(dirname ${0})
+# cd to the 'gautam_linux' directory and only work with relative paths from
+# there. This makes it is easier to add an autocommit later if necessary.
+cd /home/gautam/play/gautam_linux
 
-FOREIGN_PKGS_FILE=${DIRNAME}/foreign-packages.txt
-PACMAN_PKGS_FILE=${DIRNAME}/pacman-packages.txt
+FOREIGN_PKGS_FILE="arch-linux-packages/foreign-packages.txt"
+PACMAN_PKGS_FILE="arch-linux-packages/pacman-packages.txt"
 
 TMPFILE=$(mktemp)
 
@@ -18,8 +20,8 @@ TMPFILE=$(mktemp)
 pacman -Qqm > ${TMPFILE}
 OUTPUT=$(diff -u ${FOREIGN_PKGS_FILE} ${TMPFILE})
 if [ -n "${OUTPUT}" ]; then
-    # Note that we copy (and not move) the temp file here, as we need the temp
-    # file later when dealing with the explicitly installed packages.
+    # Note that we copy (and not move) the temp file, as we need the temp file
+    # later when dealing with the explicitly installed packages.
     cp ${TMPFILE} ${FOREIGN_PKGS_FILE}
     echo "Foreign packages list updated with:"
     echo "\`\`\`diff"
@@ -41,6 +43,30 @@ if [ -n "${OUTPUT}" ]; then
     echo "\`\`\`"
 else
     echo "No changes to the pacman packages list."
+fi
+
+# Add an autocommit if either of the package lists are modified. But only
+# do that if nothing is already staged in order to avoid inadvertently
+# auto-committing something unrelated.
+git diff --cached --quiet
+if [ $? -eq 0 ]; then
+    git diff --exit-code --no-patch ${FOREIGN_PKGS_FILE}
+    if [ $? -ne 0 ]; then
+        git add ${FOREIGN_PKGS_FILE}
+    fi
+
+    git diff --exit-code --no-patch ${PACMAN_PKGS_FILE}
+    if [ $? -ne 0 ]; then
+        git add ${PACMAN_PKGS_FILE}
+    fi
+
+    git commit --no-gpg-sign \
+        --message="[AUTOCOMMIT] update lists of installed packages"
+
+    # A nice message about the commit gets logged automatically, so we don't
+    # need to explicitly echo anything. How convenient!
+else
+    echo "Will not add an autocommit as staged files exist."
 fi
 
 echo "-------------------------------------------------------------------------"
