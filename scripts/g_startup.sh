06f87cc1c7b984effd47eb9fb297a4072e9feabe
Gautam Kotian <gkotian@users.noreply.github.com>
2020-02-28 09:37:52 +0100

    Launch update-packages-lists.sh at startup

--- scripts/g_startup.sh
+++ scripts/g_startup.sh
@@ -4,6 +4,23 @@ IS_LAPTOP="YES"
 
 GL_DIR=/home/gautam/play/gautam_linux
 
+launch_one_shot_scripts() {
+    FILE=${1}
+
+    if ! [ -f ${FILE} ]; then
+        echo "One-shot script '${FILE}' not found."
+        return 1
+    fi
+
+    ${FILE}
+    RET_CODE=$?
+
+    if [ ${RET_CODE} -ne 0 ]; then
+        echo "One-shot script '${FILE}' returned ${RET_CODE}."
+        return ${RET_CODE}
+    fi
+}
+
 # Load the SSH passphrase onto the ssh-agent.
 ssh-add ~/.ssh/id_ed25519
 
@@ -20,3 +37,5 @@ FILE=${GL_DIR}/scripts/boot_times_tracker.sh
 if [ -f ${FILE} ]; then
     ${FILE} &
 fi
+
+launch_one_shot_scripts ${GL_DIR}/arch-linux-packages/update-packages-lists.sh
