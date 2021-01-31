c7a7441ec4c139fd9e932ff03a3361645d54e614
Gautam Kotian <gkotian@users.noreply.github.com>
2021-01-31 22:05:52 +0100

    Use `deepin-screenshot` as the screenshotting tool

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# I almost always only use the interactive mode, so it makes sense to always use
-# the '-i' option. This is a script and not an alias so that I don't need to
-# open a terminal for this, but can simply call it using dmenu.
+# This is a script and not an alias so that I don't need to open a terminal for
+# this, but can simply call it using dmenu.
 
-gnome-screenshot -i
+deepin-screenshot
