28fe0cc11c1ba9247dd08f1a4e446e68b1a79f58
Gautam Kotian <gkotian@users.noreply.github.com>
2021-05-23 22:19:09 +0200

    Sleep for the first 10 minutes

--- scripts/refresh_pacman_mirrorlist/refresh_pacman_mirrorlist.sh
+++ scripts/refresh_pacman_mirrorlist/refresh_pacman_mirrorlist.sh
@@ -2,7 +2,19 @@
 
 exec 1>>/var/tmp/refresh-pacman-mirrorlist.log 2>&1
 
+# Simply sleep for the first 10 minutes. This is to allow for any VPN
+# connection, if present, to get activated.
+sleep 600
+
 echo "[`date`]"
+
+echo -n "Checking if the internet is reachable... "
+wget --quiet --tries=10 --timeout=20 --spider archlinux.org
+if [[ $? -ne 0 ]]; then
+    echo "NO"
+    exit 1
+fi
+echo "YES"
 
 OLD_REFRESH_TIME=$(grep When /etc/pacman.d/mirrorlist | cut -d":" -f2-)
 echo "Old mirrorlist refresh time: ${OLD_REFRESH_TIME}"
