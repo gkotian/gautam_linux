46c15ddb5f1a23cdbd57de2e091c9677973a8b75
Gautam Kotian <gkotian@users.noreply.github.com>
2023-08-27 21:40:13 +0200

    Support more advanced opening mechanisms

--- scripts/opener.sh
+++ scripts/opener.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# When opening a file is a matter of simply passing the file name as a command
+# line argument to the app.
 declare -A EXTENSION_APP_MAPPINGS=(
     ["go"]="bat"
     ["pl"]="bat"
@@ -21,6 +23,16 @@ declare -A EXTENSION_APP_MAPPINGS=(
     ["mp4"]="mpv"
 )
 
+# When opening a file is a bit more involved and needs to be done with the help
+# of a custom function for the specific file type.
+declare -A EXTENSION_FUNCTION_MAPPINGS=(
+    ["pfx"]="show_pfx_info"
+)
+
+show_pfx_info() {
+    openssl pkcs12 -in ${1} -info
+}
+
 if [ $# -ne 1 ]; then
     echo "Expected exactly one argument - the file to open, got $#. Aborting."
     exit 1
@@ -36,8 +48,13 @@ EXTENSION=${FILE##*.}
 
 APP=${EXTENSION_APP_MAPPINGS[${EXTENSION}]}
 if [ -z "${APP}" ]; then
-    echo "Don't know how to open '${FILE}'..."
-    exit 1
+    FUNCTION=${EXTENSION_FUNCTION_MAPPINGS[${EXTENSION}]}
+    if [ -z "${FUNCTION}" ]; then
+        echo "Don't know how to open '${FILE}'..."
+        exit 1
+    fi
+
+    ${FUNCTION} "${FILE}"
 fi
 
 ${APP} "${FILE}"
