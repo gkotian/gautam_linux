7d3052297d1af13491f1aac1815f00908c614a99
Gautam Kotian <gkotian@users.noreply.github.com>
2025-02-10 11:45:16 +0100

    Add support for manual runs

--- scripts/refresh_arch_linux_mirrorlist.sh
+++ scripts/refresh_arch_linux_mirrorlist.sh
@@ -8,6 +8,25 @@ exec 1>>${LOG_FILE} 2>&1
 
 echo "[`date`]"
 
+if [ $# -gt 1 ]; then
+    echo "ERROR: Expected at most 1 argument, got $#"
+    exit 1
+fi
+
+# During automatic runs, we sleep for 10 minutes in the beginning to allow for
+# any VPN connection, if present, to get activated.
+INITIAL_SLEEP_TIME=600
+
+if [ $# -eq 1 ]; then
+    if [ "$1" != "--manual" ]; then
+        echo "ERROR: Invalid argument '$1'. Only '--manual' is accepted."
+        exit 1
+    else
+        echo "Performing a manual run"
+        INITIAL_SLEEP_TIME=0
+    fi
+fi
+
 MIRRORLIST=${HOME}/.config/arch_linux_mirrorlist
 
 if [ -f "${MIRRORLIST}" ]; then
@@ -31,9 +50,7 @@ fi
 
 echo "Will update the mirrorlist"
 
-# Simply sleep for the first 10 minutes. This is to allow for any VPN
-# connection, if present, to get activated.
-sleep 600
+sleep ${INITIAL_SLEEP_TIME}
 
 echo -n "Checking if the internet is reachable... "
 wget --quiet --tries=10 --timeout=20 --spider archlinux.org
