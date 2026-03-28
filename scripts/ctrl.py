6fa4e4781b16da99568330c0b84e14ccab813126
Gautam Kotian <gkotian@users.noreply.github.com>
2026-03-28 22:32:01 +0100

    Print timestamp

--- scripts/ctrl.py
+++ scripts/ctrl.py
@@ -2,13 +2,15 @@
 
 """Simulates a Control key press every minute."""
 
+import datetime
 import subprocess
 import time
 
 def main():
     while True:
+        print(datetime.datetime.now().isoformat(timespec='seconds'))
         subprocess.run(["xdotool", "key", "ctrl"], check=True)
-        time.sleep(60)
+        time.sleep(500)
 
 if __name__ == "__main__":
     main()
